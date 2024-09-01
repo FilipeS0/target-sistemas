5bb08ae3940478812eafbc80aca15fc361c88005
Filipe Souto <filipesoutocosta@gmail.com>
2024-09-01 15:55:35 -0300

    starting fibo

---
+++ tec2.py
@@ -0,0 +1,12 @@
+# 2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
+
+# IMPORTANTE: Esse número pode ser informado através de qualquer entrada de sua preferência ou pode ser previamente definido no código;
+
+def fibo(n):
+    
+
+
+
+# Pegue o valor
+e_fibo = int(input('Qual numero voce quer checar se esta na sequencia fibonacci?'))
+fibo(e_fibo)
